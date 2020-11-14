1c3eb10266da623abe17b62e19cc28508c5f99ef
alayor <alayor3@gmail.com>
2020-11-14 10:33:57 -0700

    Make the endpoint /contracts/:id return the contract only if it belongs to the profile.

--- src/app.js
+++ src/app.js
@@ -1,21 +1,30 @@
-const express = require('express');
-const bodyParser = require('body-parser');
-const {sequelize} = require('./model')
-const {getProfile} = require('./middleware/getProfile')
-const app = express();
-app.use(bodyParser.json());
+const express = require('express')
+const bodyParser = require('body-parser')
+const Sequelize = require('sequelize')
+const { sequelize } = require('./model')
+const { getProfile } = require('./middleware/getProfile')
+const app = express()
+app.use(bodyParser.json())
 app.set('sequelize', sequelize)
 app.set('models', sequelize.models)
+const Op = Sequelize.Op
+sequelize.sync({ logging: console.log })
 
 /**
- * FIX ME!
+ * Returns the contract that belongs to the profile.
  * @returns contract by id
  */
-app.get('/contracts/:id',getProfile ,async (req, res) =>{
-    const {Contract} = req.app.get('models')
-    const {id} = req.params
-    const contract = await Contract.findOne({where: {id}})
-    if(!contract) return res.status(404).end()
-    res.json(contract)
+app.get('/contracts/:id', getProfile, async (req, res) => {
+  const { Contract } = req.app.get('models')
+  const { id } = req.params
+  const { id: profileId } = req.profile
+  const contract = await Contract.findOne({
+    where: {
+      id,
+      [Op.or]: [{ ClientId: profileId }, { ContractorId: profileId }],
+    },
+  })
+  if (!contract) return res.status(404).end()
+  res.json(contract)
 })
-module.exports = app;
+module.exports = app
